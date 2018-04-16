ba46bc53ad2224ce4dfaa6f98f73d6359f0534e8
tianyongping <yongping.tian@okcoin.com>
2018-04-16 16:34:54 +0800

    modify log.go.

--- log.go
+++ log.go
@@ -9,46 +9,14 @@ import (
 */
 func CheckAndPrintError(flag string, err error) {
 	if err != nil {
-		log.Fatal(flag+"\n", err)
+		log.Println(flag+"\n", err)
 	}
 }
 
 func Print(arg ...interface{}) {
-	log.Fatal(arg)
+	log.Print(arg)
 }
 
-func PrintSlowConn(driverName, host, dbName string, consume int64) {
-	builder := NewStringBuilder()
-	builder.Append(driverName)
-	builder.Append("Conn Timeout: ")
-	builder.Append("Host: ")
-	builder.Append(host)
-	builder.Append(", DBName: ")
-	builder.Append(dbName)
-	builder.Append("Consume time: ")
-	builder.Append(NewStringInt64(consume).ToString())
-	builder.Append("ms")
-	log.Fatal(builder.ToString())
-}
-
-func PrintErrorSql(driverName string, err error, sql string, args ...interface{}) {
-	if err != nil {
-		log.Fatal(driverName+"Error Sql: ", sql, args)
-	}
-}
-
-func PrintSlowSql(driverName, host, dbName string, consume int64, sql string, args ...interface{}) {
-	builder := NewStringBuilder()
-	builder.Append(driverName)
-	builder.Append("Slow Sql: ")
-	builder.Append("Host: ")
-	builder.Append(host)
-	builder.Append(", DBName: ")
-	builder.Append(dbName)
-	builder.Append("Consume time: ")
-	builder.Append(NewStringInt64(consume).ToString())
-	builder.Append("ms, Sql: ")
-	builder.Append(sql)
-	builder.Append(", Args: ")
-	log.Fatal(builder.ToString(), args)
+func Println(arg ...interface{}) {
+	log.Println(arg)
 }
