2b958ffc2948c90a07d002084bb63a53a49aaf17
tianyongping <yongping.tian@okcoin.com>
2018-04-16 16:40:08 +0800

    add string builder construction method.

--- string_builder.go
+++ string_builder.go
@@ -41,6 +41,28 @@ func (builder *StringBuilder) Append(s string) *StringBuilder {
 	return builder
 }
 
+func (builder *StringBuilder) AppendStrings(ss ... string) *StringBuilder {
+	for i := range ss {
+		builder.buffer.WriteString(ss[i])
+	}
+	return builder
+}
+
+func (builder *StringBuilder) AppendInt(i int) *StringBuilder {
+	builder.buffer.WriteString(NewStringInt(i).ToString())
+	return builder
+}
+
+func (builder *StringBuilder) AppendInt64(i int64) *StringBuilder {
+	builder.buffer.WriteString(NewStringInt64(i).ToString())
+	return builder
+}
+
+func (builder *StringBuilder) AppendFloat64(f float64) *StringBuilder {
+	builder.buffer.WriteString(NewStringFloat64(f).ToString())
+	return builder
+}
+
 func (builder *StringBuilder) Replace(old, new string) *StringBuilder {
 	str := NewString(builder.ToString()).Replace(old, new)
 	builder.Clear()
